083b2265f67a4deea7207be98fb6ab754f388624
Florian Sihler <florian.sihler@uni-ulm.de>
2022-08-14 14:40:44 +0200

    format build.lua

--- build.lua
+++ build.lua
@@ -1,10 +1,10 @@
 #!/usr/bin/env texlua
+
 -- Execute with ======================================================
 -- l3build tag
 -- l3build ctan
 -- l3build upload
 -- l3build clean
-
 -- Settings ==========================================================
 bundle = ""
 module = "tikzpingus"
@@ -18,14 +18,14 @@ handle:close()
 newsubtag = string.sub(oldtag, 4)
 newmajortag = string.sub(oldtag, 0, 3)
 if (options["target"] == "tag") then
-	newsubtag = newsubtag + 1
+    newsubtag = newsubtag + 1
 end
 packageversion = newmajortag .. math.floor(newsubtag)
---packageversion="v1.3"
+-- packageversion="v1.3"
 
 -- Package date ======================================================
 packagedate = os.date("!%Y-%m-%d")
---packagedate = "2020-01-02"
+-- packagedate = "2020-01-02"
 
 -- interacting with git ==============================================
 function git(...)
@@ -37,48 +37,42 @@ function git(...)
 end
 
 -- replace version tags in .sty and -doc.tex files ===================
-tagfiles = { "*.sty", "doc/*.tex" }
-function update_tag (file,content,tagname,tagdate)
-	tagdate = string.gsub (packagedate,"-", "/")
-	if string.match (file, "%.sty$" ) then
-		content = string.gsub (
-			content,
-			"\\ProvidesPackage{(.-)}%[%d%d%d%d%/%d%d%/%d%d version v%d%.%d+",
-			"\\ProvidesPackage{%1}[" .. tagdate.." version "..packageversion
-		)
-		return content
-	elseif string.match (file, "doc/*.tex$" ) then
-		content = string.gsub (
-			content,
-			"\\date{Version v%d%.%d+ \\textendash\\ %d%d%d%d%/%d%d%/%d%d",
-			"\\date{Version " .. packageversion .. " \\textendash{} " .. tagdate
-		)
-		return content
-	end
-	return content
+tagfiles = {"*.sty", "doc/*.tex"}
+function update_tag(file, content, tagname, tagdate)
+    tagdate = string.gsub(packagedate, "-", "/")
+    if string.match(file, "%.sty$") then
+        content = string.gsub(content, "\\ProvidesPackage{(.-)}%[%d%d%d%d%/%d%d%/%d%d version v%d%.%d+",
+            "\\ProvidesPackage{%1}[" .. tagdate .. " version " .. packageversion)
+        return content
+    elseif string.match(file, "doc/*.tex$") then
+        content = string.gsub(content, "\\date{Version v%d%.%d+ \\textendash\\ %d%d%d%d%/%d%d%/%d%d",
+            "\\date{Version " .. packageversion .. " \\textendash{} " .. tagdate)
+        return content
+    end
+    return content
 end
 
 -- committing retagged file and tag the commit =======================
 function tag_hook(tagname)
-	git("add", "*.sty")
-	git("add", "doc/*.tex")
-	git("commit -m 'step version " .. packageversion .. "'" )
-	git("tag", packageversion)
+    git("add", "*.sty")
+    git("add", "doc/*.tex")
+    git("commit -m 'step version " .. packageversion .. "'")
+    git("tag", packageversion)
 end
 
 -- collecting files for ctan =========================================
-docfiledir    = "./doc"
+docfiledir = "./doc"
 sourcefiledir = "./tex"
 
-docfiles = {"*","build/"..module.."-doc.pdf"}
+docfiles = {"*", "build/" .. module .. "-doc.pdf"}
 indexstyle = {"doc/indexstyle.ist"}
 
-textfiles= {"README.md"}
-ctanreadme= "README.md"
+textfiles = {"README.md"}
+ctanreadme = "README.md"
 
 installfiles = {"*.sty", "*.tex"}
 sourcefiles = installfiles
-unpackfiles  = { }
+unpackfiles = {}
 
 excludefiles = {"sub_*.pdf"}
 
@@ -91,19 +85,19 @@ flatten = false
 require('build-private.lua')
 
 uploadconfig = {
-  author       = uploadconfig.author,
-  uploader     = uploadconfig.uploader,
-  email        = uploadconfig.email,
-  pkg          = ctanpkg,
-  version      = packageversion .. " " .. packagedate,
-  license      = "gpl3.0",
-  summary      = "Penguins with TikZ",
-  ctanPath     = "/graphics/pgf/contrib/" .. ctanpkg,
-  repository   = "https://github.com/EagleoutIce/" .. module,
-  note         = [[Uploaded automatically by l3build...]],
-  bugtracker   = "https://github.com/EagleoutIce/" .. module .. "/issues",
-  support      = "https://github.com/EagleoutIce/" .. module .. "/issues",
-  announcement_file = "announcement.txt"
+    author = uploadconfig.author,
+    uploader = uploadconfig.uploader,
+    email = uploadconfig.email,
+    pkg = ctanpkg,
+    version = packageversion .. " " .. packagedate,
+    license = "gpl3.0",
+    summary = "Penguins with TikZ",
+    ctanPath = "/graphics/pgf/contrib/" .. ctanpkg,
+    repository = "https://github.com/EagleoutIce/" .. module,
+    note = [[Uploaded automatically by l3build...]],
+    bugtracker = "https://github.com/EagleoutIce/" .. module .. "/issues",
+    support = "https://github.com/EagleoutIce/" .. module .. "/issues",
+    announcement_file = "announcement.txt"
 }
 
 -- cleanup ===========================================================
